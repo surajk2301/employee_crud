fd51660b85016fc9278aa96690d08ade75138fd8
Suraj Karande <surajdkarande6396@gmail.com>
2025-11-05 17:48:58 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Dashboard from "./Pages/Dashboard";
+import EmployeeProfile from "./Pages/EmployeeProfile";
+import Login from "./Pages/Login";
+import Register from "./pages/Register";
+import Welcome from "./pages/Welcome";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Welcome />}></Route>
+        <Route path="/login" element={<Login/>}></Route>
+        <Route path="/register" element={<Register/>}></Route>
+
+        <Route path="/dashboard" element={<Dashboard/>}></Route>
+
+        <Route path="/employee-profile/:id" element={<EmployeeProfile />}></Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
